714466c869a62113c19dfc134d145a5959401940
Keerthikumarreddy-kanchamreddy <122507926+Keerthikumarreddy-kanchamreddy@users.noreply.github.com>
2024-04-15 15:44:02 -0400

    displaying the question

--- Quiz App/quiz.js
+++ Quiz App/quiz.js
@@ -3,7 +3,16 @@ const questionObj =
       category: 'Food & Drink',
       id: 'qa-1',
       correctAnswer: 'Three',
-      answers: ['Two', 'Three ', 'Four', 'Five'],
+      options: ['Two', 'Three ', 'Four', 'Five'],
       question:
         "How many pieces of bun are in a Mcdonald's Big Mac?",
     };
+
+// destructring the questionObj Object. 
+    const {
+      correctAnswer, options, question
+    } = questionObj;
+
+// fetching the question element from the HTML document and attaching the text content for it to be visible on the screen
+    const questionElement  = document.getElementById('question');
+    questionElement.textContent = question;
